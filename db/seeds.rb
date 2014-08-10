e7b2ea87369f91249a86acfc5d49ed199187cf2b
Seth Geyer <seth.geyer@gmail.com>
2014-08-10 09:48:23 -0600

    added a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+# User.destroy_all
+# TaskList.destroy_all
+
+
+
+user1 = User.create!(username: "seth", email: "seth@gmail.com", ssn: "223334444", is_admin: true, password: 'password')
+account1 = Account.create!(user_id: user1.id)
+deposit1 = Deposit.create!(account_id: account1.id, amount: 40000, cc_number: 111, exp_date: "2019-07-15", name_on_card: "Seth Geyer", cc_type: "Visa")
+
+user2 = User.create!(username: "alex", email: "alex@gmail.com", ssn: "112223333", is_admin: false, password: 'password')
+account2 = Account.create!(user_id: user2.id)
+deposit1 = Deposit.create!(account_id: account2.id, amount: 30000, cc_number: 222, exp_date: "2019-07-15", name_on_card: "Alex McRitchie", cc_type: "Visa")
+
+user3 = User.create!(username: "stan", email: "stan@gmail.com", ssn: "334445555", is_admin: false, password: 'password')
+account3 = Account.create!(user_id: user3.id)
+deposit1 = Deposit.create!(account_id: account3.id, amount: 30000, cc_number: 333, exp_date: "2019-07-15", name_on_card: "Stan Smith", cc_type: "Visa")
+
+charity1 = Charity.create!(name: "United Way", tax_id: 333, poc: "Ulysess Williams", poc_email: "uw@unitedway.com", status: "registered")
+charity2 = Charity.create!(name: "Red Cross", tax_id: 444, poc: "Ray Crumb", poc_email: "rc@redcross.com", status: "registered")
+
