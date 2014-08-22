64af03911fcbaa2607179b99f5751fe0b59085f1
Seth Geyer <seth.geyer@gmail.com>
2014-08-22 12:17:34 -0600

    refactored the references to wagerer/wageree_outcomes to winner_id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,9 +38,10 @@ class UsersController < ApplicationController
       ###################
       @deposit_total = @account.deposits.sum(:amount) / 100
       @distribution_total = @account.distributions.sum(:amount) / 100
-      @wagered_total = (@account.proposed_wagers.where(wageree_outcome: nil).where(wagerer_outcome: nil).sum(:amount) / 100) + (ProposedWager.where(wageree_id: kenny_loggins.id, status: "accepted").where(wageree_outcome: nil).where(wagerer_outcome: nil).sum(:amount) / 100)
+      @wagered_total = (@account.proposed_wagers.where(winner_id: nil).sum(:amount) / 100) + (ProposedWager.where(wageree_id: kenny_loggins.id, status: "accepted").where(winner_id: nil).sum(:amount) / 100)
 
-      @winnings_total = (@account.proposed_wagers.where(wageree_outcome: "I Lost").where(status: "completed").sum(:amount) / 100) + (ProposedWager.where(wageree_id: kenny_loggins.id, status: "completed").where(wagerer_outcome: "I Lost").sum(:amount) / 100) - ( (@account.proposed_wagers.where(wagerer_outcome: "I Lost").where(status: "completed").sum(:amount) / 100) + (ProposedWager.where(wageree_id: kenny_loggins.id, status: "completed").where(wageree_outcome: "I Lost").sum(:amount) / 100))
+      # @winnings_total = (@account.proposed_wagers.where(winner_id: kenny_loggins.id).sum(:amount) / 100) + (ProposedWager.where(wageree_id: kenny_loggins.id).where(winner_id: kenny_loggins.id).sum(:amount) / 100) - ( (@account.proposed_wagers.where('winner_id != ?', kenny_loggins.id).sum(:amount) / 100) + (ProposedWager.where(wageree_id: kenny_loggins.id).where('winner_id != ?', kenny_loggins.id).sum(:amount) / 100))
+      @winnings_total = (ProposedWager.where(winner_id: kenny_loggins.id).sum(:amount) / 100) - ( (@account.proposed_wagers.where('winner_id != ?', kenny_loggins.id).sum(:amount) / 100) + (ProposedWager.where(wageree_id: kenny_loggins.id).where('winner_id != ?', kenny_loggins.id).sum(:amount) / 100))
 
       @net_amount = @deposit_total - @distribution_total - @wagered_total + @winnings_total
       @proposed_wagers = @account.proposed_wagers + ProposedWager.where(wageree_id: kenny_loggins.id)
