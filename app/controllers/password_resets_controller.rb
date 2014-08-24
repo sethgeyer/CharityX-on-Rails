cdd70d4defe8a1bc02324e4bafb7f9367db6a258
Seth Geyer <seth.geyer@gmail.com>
2014-08-24 07:26:56 -0600

    added secure random number to password reset url

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -9,7 +9,7 @@ class PasswordResetsController < ApplicationController
   def create
     @password_reset_request = PasswordReset.new
     @password_reset_request.email = params[:password_reset][:email]
-    @password_reset_request.unique_identifier = 5
+    @password_reset_request.unique_identifier = SecureRandom.uuid
     @password_reset_request.expiration_date = Time.now + 1.day
     if @password_reset_request.save
       flash[:notice] = "Password reset instructions have been sent to #{@password_reset_request.email}"
