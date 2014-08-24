57eda525cafea076c767f33a65181e849c0600b0
Seth Geyer <seth.geyer@gmail.com>
2014-08-23 19:32:31 -0600

    added production environment host name to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
   }
 
 
+  config.action_mailer.default_url_options = { :host => 'charity-x.heroku.com' }
+
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
