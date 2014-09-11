0a7e9eed19af965a880244b5e9d36dce914bdd1e
Seth Geyer <seth.geyer@gmail.com>
2014-09-10 19:03:57 -0600

    refactored dashboard to pass a singular instance variable

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -15,22 +15,6 @@ class DashboardsController < ApplicationController
 
     @dashboard = Dashboard.new
 
-    # @unallocated_chips = kenny_loggins.chips.where(status: "available")
-    # @distributed_chips = kenny_loggins.chips.where(status: "distributed")
-    @wagered_chips = kenny_loggins.chips.where(status: "wagered")
-
-    @deposit_total = kenny_loggins.deposits.sum(:amount) / 100
-    @distribution_total = kenny_loggins.distributions.sum(:amount) / 100
-    @wagered_total = (kenny_loggins.wagers.where(winner_id: nil).where('status != ?', 'declined').where('status != ?', 'expired').sum(:amount) / 100) + (Wager.where(wageree_id: kenny_loggins.id, status: "accepted").where(winner_id: nil).sum(:amount) / 100)
-    @winnings_total = (Wager.where(winner_id: kenny_loggins.id).sum(:amount) / 100) - ((kenny_loggins.wagers.where('winner_id != ?', kenny_loggins.id).sum(:amount) / 100) + (Wager.where(wageree_id: kenny_loggins.id).where('winner_id != ?', kenny_loggins.id).sum(:amount) / 100))
-    @net_amount = @deposit_total - @distribution_total - @wagered_total + @winnings_total
-
-    unfiltered_user_wagers = (kenny_loggins.wagers + Wager.where(wageree_id: kenny_loggins.id))
-    @registered_wagers = Wager.compile_wagers_to_view_based_on_user_preferences(kenny_loggins, unfiltered_user_wagers)
-
-    unfiltered_public_wagers = Wager.where(wageree_id: nil).where('user_id != ?', kenny_loggins.id).select { |wager| wager.non_registered_wageree == nil }
-    @public_wagers = Wager.compile_wagers_to_view_based_on_user_preferences(kenny_loggins, unfiltered_public_wagers)
   end
-
 
 end
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,13 +1,38 @@
 class Dashboard
 
 
-  def list_unallocated_chips(kenny_loggins)
-
-    kenny_loggins.chips.where(status: "available")
+  def chip_count(kenny_loggins, status)
+    kenny_loggins.chips.where(status: status).count
   end
 
-  def list_distributed_chips(kenny_loggins)
-  kenny_loggins.chips.where(status: "distributed")
+
+  def sum_of_deposits(kenny_loggins)
+    kenny_loggins.deposits.sum(:amount) / 100
   end
 
+  def sum_of_distributions(kenny_loggins)
+    kenny_loggins.distributions.sum(:amount) / 100
+  end
+
+  def sum_of_winnings(kenny_loggins)
+    (Wager.where(winner_id: kenny_loggins.id).sum(:amount) / 100) - ((kenny_loggins.wagers.where('winner_id != ?', kenny_loggins.id).sum(:amount) / 100) + (Wager.where(wageree_id: kenny_loggins.id).where('winner_id != ?', kenny_loggins.id).sum(:amount) / 100))
+  end
+
+  def sum_of_wagered(kenny_loggins)
+    (kenny_loggins.wagers.where(winner_id: nil).where('status != ?', 'declined').where('status != ?', 'expired').sum(:amount) / 100) + (Wager.where(wageree_id: kenny_loggins.id, status: "accepted").where(winner_id: nil).sum(:amount) / 100)
+  end
+
+  def sum_of_net_amount(kenny_loggins)
+    self.sum_of_deposits(kenny_loggins) - self.sum_of_distributions(kenny_loggins) - self.sum_of_wagered(kenny_loggins) + self.sum_of_winnings(kenny_loggins)
+  end
+
+  def registered_wagers(kenny_loggins)
+    unfiltered_user_wagers = (kenny_loggins.wagers + Wager.where(wageree_id: kenny_loggins.id))
+    Wager.compile_wagers_to_view_based_on_user_preferences(kenny_loggins, unfiltered_user_wagers)
+  end
+
+  def public_wagers(kenny_loggins)
+    unfiltered_public_wagers = Wager.where(wageree_id: nil).where('user_id != ?', kenny_loggins.id).select { |wager| wager.non_registered_wageree == nil }
+    Wager.compile_wagers_to_view_based_on_user_preferences(kenny_loggins, unfiltered_public_wagers)
+  end
 end
