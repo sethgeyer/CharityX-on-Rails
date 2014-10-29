9d355cc37998148e7a20ba1f52acf8a96831e643
Seth Geyer <seth.geyer@gmail.com>
2014-10-29 14:37:21 -0600

    cleaned up spacing on edit user profile spec

--- spec/features/user_edits_profile_spec.rb
+++ spec/features/user_edits_profile_spec.rb
@@ -1,22 +1,20 @@
-require 'rails_helper'
-require 'capybara/rails'
-
-
-
 feature "editing user profile" do
+
   before(:each) do
-    fill_in_registration_form("Stephen")
+    create_user("Stephen")
+    visit "/"
+    login_user("Stephen")
   end
 
   scenario "user updates their user profile" do
     click_on "Edit Profile"
-    fill_in "Email", with: "steve@gmail.com"
+    fill_in "Email", with: "steven@gmail.com"
     fill_in "Password", with: "stephen1"
     click_on "Submit"
     expect(page).to have_content("Your changes have been saved")
     expect(page).to have_css("#show_dashboards")
     click_on "Edit Profile"
-    expect(page).to have_selector("input[value='steve@gmail.com']")
+    expect(page).to have_selector("input[value='steven@gmail.com']")
   end
 
   scenario "user leaves email blank when editing" do
@@ -27,7 +25,6 @@ feature "editing user profile" do
     expect(page).to have_content("can't be blank")
   end
 
-
   scenario "user leaves password blank when editing" do
     click_on "Edit Profile"
     fill_in "Password", with: "sds"
@@ -35,14 +32,6 @@ feature "editing user profile" do
     expect(page).to have_css("#edit_profiles")
     expect(page).to have_content("Password must be at least 7 characters")
   end
-
-
-  # scenario "anyone besides the user can not change the individual's profile via the direct url" do
-  #   visit "/users/5000/edit"
-  #
-  #   expect(page).to have_content("You are not authorized to visit this page")
-  #   expect(page).to have_css("#homepage")
-  # end
 
   scenario "user decides to cancel their edits" do
     click_on "Edit Profile"
