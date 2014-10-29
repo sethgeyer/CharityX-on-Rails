3fbebec77430807e0f90d991ca9c01630d4666a6
Seth Geyer <seth.geyer@gmail.com>
2014-10-29 16:29:45 -0600

    tidyed up spacing on profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,26 +1,12 @@
 class ProfilesController < ApplicationController
   def edit
-    # if kenny_loggins == User.find(params[:id])
-      @user = kenny_loggins  #<--- no test written to test whether a sessioned user can view someone else's view
-      render :edit
-    # else
-    #   flash[:notice] = "You are not authorized to visit this page"
-    #   redirect_to root_path
-    # end
-
+    @user = kenny_loggins #<--- no test written to test whether a sessioned user can view someone else's view
   end
 
   def update
     @user = kenny_loggins
-    @user.email = params[:user][:email].downcase
-    @user.first_name = params[:user][:first_name]
-    @user.last_name = params[:user][:last_name]
-
-    if params[:user][:password] != ""
-      @user.password = params[:user][:password]
-
-    end
-
+    @user.update(allowed_params)
+    @user.password = params[:user][:password] if params[:user][:password] != ""
     if @user.save
       flash[:notice] = "Your changes have been saved"
       redirect_to user_dashboard_path
@@ -28,4 +14,17 @@ class ProfilesController < ApplicationController
       render :edit
     end
   end
+
+  private
+
+  def allowed_params
+    params.require(:user).permit(
+      :email,
+      :first_name,
+      :last_name,
+    )
+  end
+
+
+
 end
