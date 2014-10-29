47d0acd39ac5606f82f7dc3f551e738dc6a39991
Seth Geyer <seth.geyer@gmail.com>
2014-10-29 16:39:15 -0600

    inlined a 'new' wager view preference

--- app/controllers/wager_view_preferences_controller.rb
+++ app/controllers/wager_view_preferences_controller.rb
@@ -1,10 +1,7 @@
 class WagerViewPreferencesController < ApplicationController
 
   def create
-    wager_view_preference = WagerViewPreference.new
-    wager_view_preference.user_id = kenny_loggins.id
-    wager_view_preference.wager_id = params[:wager_id]
-    wager_view_preference.show = false
+    wager_view_preference = kenny_loggins.wager_view_preferences.new(wager_id: params[:wager_id], show: false)
     wager_view_preference.save!
     render nothing: true
   end
