62009087d3b0cc23d8a0ce633f86bc2978be2464
Seth Geyer <seth.geyer@gmail.com>
2014-11-09 06:37:21 -0700

    refactored jscript and bundled actions under named variable functions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,120 +19,81 @@
 //= require picker.time
 
 
-
-$(document).ready( function() {
-
+$(document).ready(function () {
 
 
-
-//  var inputGameID= $('.game-id-container').val()
-//  if (inputGameID != "") {
-//    $('.wager-date-input').prop('readonly', true)
-//    $('.wager-details-input').prop('readonly', true)
-//    $('.wager-input').prop('readonly', true)
-//    $('#datetimepicker6').data("DateTimePicker").disable();
-//
-//    $('.wager-date-input').removeProp('disabled');
-//    $('.wager-date-input').prop('readonly', true);
-//  }
-
-
-
-$('.btn-success').on('click', function() {
-  $('#carousel-example-generic').show()
-})
-
-
-
+  $('.btn-success').on('click', function () {
+    $('#carousel-example-generic').show();
+  })
 
 
 //  User can select sport games from the modal and the information is auto-populated into the wager form
-  $('.modal-body a').on('click', function(event) {
-    event.preventDefault()
+  $('.modal-body a').on('click', function (event) {
+    event.preventDefault();
 
-    var $selectedTeam = $(this)
+    var $selectedTeam = $(this);
 
-    var selectedTeamName = $selectedTeam.text()
-    var selectedWinnerID = $selectedTeam.attr('data-team-id')
+    var selectedTeamName = $selectedTeam.text();
+    var selectedWinnerID = $selectedTeam.attr('data-team-id');
 
-    var loserName = $selectedTeam.siblings('a')[0].text
-    var gameContainer = $selectedTeam.parents('.game-container')
-    var gameID = gameContainer.attr('data-game-id')
-    var gameWeek = gameContainer.attr('data-game-week')
+    var loserName = $selectedTeam.siblings('a')[0].text;
+    var gameContainer = $selectedTeam.parents('.game-container');
+    var gameID = gameContainer.attr('data-game-id');
+    var gameWeek = gameContainer.attr('data-game-week');
 
-    var homeTeam = gameContainer.attr('data-home-team-id')
-    var visitingTeam = gameContainer.attr('data-vs-team-id')
+    var homeTeam = gameContainer.attr('data-home-team-id');
+    var visitingTeam = gameContainer.attr('data-vs-team-id');
 
-    var inputWagerField = $('.wager-input')
-    var inputWagerDateField = $('.wager-date-input')
-    var inputWagerTimeField = $('.wager-time-input')
-    var inputWagerDetailsField = $('.wager-details-input')
+    var inputWagerField = $('.wager-input');
+    var inputWagerDateField = $('.wager-date-input');
+    var inputWagerTimeField = $('.wager-time-input');
+    var inputWagerDetailsField = $('.wager-details-input');
 
-    inputWagerField.val('The ' + selectedTeamName + ' beat the ' + loserName)
 
-    var gameDate = gameContainer.attr('data-game-date')
-    var gameTime = gameContainer.attr('data-game-time')
+    var gameDate = gameContainer.attr('data-game-date');
+    var gameTime = gameContainer.attr('data-game-time');
 
-    var gameTemperature = gameContainer.attr('data-game-temperature')
-    var gameCondition = gameContainer.attr('data-game-condition')
-    var gameLocation = gameContainer.attr('data-game-location')
+    var gameTemperature = gameContainer.attr('data-game-temperature');
+    var gameCondition = gameContainer.attr('data-game-condition');
+    var gameLocation = gameContainer.attr('data-game-location');
 
     if (gameCondition != "") {
-      var gameForecast = " / Forecast: " + gameTemperature + " and " + gameCondition
+      var gameForecast = " / Forecast: " + gameTemperature + " and " + gameCondition;
     }
     else {
-      var gameForecast = ""
+      var gameForecast = "";
     }
-    inputWagerDateField.val(gameDate)
-    inputWagerTimeField.val(gameTime)
 
+    var setWagerInputFieldsOnForm = function() {
+      inputWagerField.val('The ' + selectedTeamName + ' beat the ' + loserName);
+      inputWagerDateField.val(gameDate);
+      inputWagerTimeField.val(gameTime);
+      inputWagerField.siblings('.game-id-container').val(gameID);
+      inputWagerField.siblings('.selected-winner-container').val(selectedWinnerID);
+      inputWagerField.siblings('.week-container').val(gameWeek);
+      inputWagerField.siblings('.home-id-container').val(homeTeam);
+      inputWagerField.siblings('.visitor-id-container').val(visitingTeam);
+      inputWagerDetailsField.val("@" + gameLocation + gameForecast);
+    }
 
+    setWagerInputFieldsOnForm();
 
-    inputWagerField.siblings('.game-id-container').val(gameID)
-    inputWagerField.siblings('.selected-winner-container').val(selectedWinnerID)
-    inputWagerField.siblings('.week-container').val(gameWeek)
+    var lockDownInputFieldsOnForm = function() {
+      inputWagerDateField.prop('readonly', true);
+      inputWagerField.prop('readonly', true);
+      inputWagerDetailsField.prop('readonly', true);
+    }
 
-    inputWagerField.siblings('.home-id-container').val(homeTeam)
-    inputWagerField.siblings('.visitor-id-container').val(visitingTeam)
-
-
-
-
-    inputWagerDetailsField.val("@" + gameLocation + gameForecast)
-
-
-
-//    $('#datetimepicker6').data("DateTimePicker").disable();
-
-//    inputWagerDateField.removeProp('disabled');
-
-    $('.wager-date-input').pickadate({
-      disablePicker: true
-    })
-
-
-    inputWagerDateField.prop('readonly', true);
-    inputWagerField.prop('readonly', true);
+    lockDownInputFieldsOnForm()
 
     $('#myModal').modal("hide")
-    inputWagerDetailsField.prop('readonly', true)
 
   })
 
 
-
-
-
-
-
-
-
-
-
-
 //  Show an outcome flag based on whether the match has been won, lost or is still pending and highlight the row if the due date has passed.
   var wagers = $('#wagers_table .wager-border');
-  wagers.each(function() {
+  wagers.each(function () {
     $wager = $(this);
 
 
@@ -143,26 +104,24 @@ $('.btn-success').on('click', function() {
 
     if (outcome.html() == null && date < 0) {
       $wager.addClass('past-due-outcome');
-      $wager.find(".wager-actions").append("Has the outcome been determined?")
-      $wager.find(".wager-actions").append("Did you Win this bet?")
-    };
-
+      $wager.find(".wager-actions").append("Has the outcome been determined?");
+      $wager.find(".wager-actions").append("Did you Win this bet?");
+    }
   });
 
-  $('.expand-icon').on('click', function() {
-    $(this).toggleClass('glyphicon-th-list').toggleClass('glyphicon-circle-arrow-up')
+  $('.expand-icon').on('click', function () {
+    $(this).toggleClass('glyphicon-th-list').toggleClass('glyphicon-circle-arrow-up');
     $(this).parents('.wager-border').find('.wager-details').toggle();
     $(this).parents('.wager-border').siblings().find('.wager-details').hide();
     $(this).parents('.wager-border').siblings().find('.expand-icon').removeClass('glyphicon-circle-arrow-up').addClass('glyphicon-th-list');
-
   });
 
 
-  $('.table-expand-icon').on('click', function() {
-    $(this).toggleClass('glyphicon-th-list').toggleClass('glyphicon-circle-arrow-up')
-    var contentsSection = $(this).parents('.blue-bordered').children('.summary-total')
-    contentsSection.toggleClass('expanded-contents')
-    var $selectedDropDownDetail = $(this).parents('.blue-bordered').children('.drop-down-detail')
+  $('.table-expand-icon').on('click', function () {
+    $(this).toggleClass('glyphicon-th-list').toggleClass('glyphicon-circle-arrow-up');
+    var contentsSection = $(this).parents('.blue-bordered').children('.summary-total');
+    contentsSection.toggleClass('expanded-contents');
+    var $selectedDropDownDetail = $(this).parents('.blue-bordered').children('.drop-down-detail');
     $selectedDropDownDetail.toggle();
   });
 
@@ -172,55 +131,43 @@ $('.btn-success').on('click', function() {
 //    $(this).parents('.wager-border').siblings().find('.expand-icon').removeClass('glyphicon-circle-arrow-up').addClass('glyphicon-th-list');
 
 
-
-
-  $('.delete-icon').on('click', function() {
+  $('.delete-icon').on('click', function () {
     var $xButton = $(this)
-    var archiveMessage = "<div class='archive' style='color:red'>This wager is being archived</div><br/>"
-    var wagerID = $xButton.attr('data-id')
-    var postWagerPreferencePromise = $.post("/user/wager_view_preferences", {wager_id: wagerID})
+    var archiveMessage = "<div class='archive' style='color:red'>This wager is being archived</div><br/>";
+    var wagerID = $xButton.attr('data-id');
+    var postWagerPreferencePromise = $.post("/user/wager_view_preferences", {wager_id: wagerID});
 
-
-    postWagerPreferencePromise.success( function() {
-
+    postWagerPreferencePromise.success(function () {
       $xButton.parents('.wager-border').after(archiveMessage);
-      $('.archive').fadeOut(2000)
-      $xButton.parents('.wager-border').fadeOut(2000)
-
-
-
+      $('.archive').fadeOut(2000);
+      $xButton.parents('.wager-border').fadeOut(2000);
     });
 
-
   });
 
 
-
-  $('.wager-title').on('mouseover', function() {
-    $(this).toggleClass('active-wager')
+  $('.wager-title').on('mouseover', function () {
+    $(this).toggleClass('active-wager');
   });
 
-  $('.wager-title').on('mouseleave', function() {
-    $(this).toggleClass('active-wager')
+  $('.wager-title').on('mouseleave', function () {
+    $(this).toggleClass('active-wager');
   })
 
 
-
-
-  $('.button').on('mouseover', function() {
-    $(this).toggleClass('active-button')
+  $('.button').on('mouseover', function () {
+    $(this).toggleClass('active-button');
   });
 
-  $('.button').on('mouseleave', function() {
-    $(this).toggleClass('active-button')
+  $('.button').on('mouseleave', function () {
+    $(this).toggleClass('active-button');
   })
 
 
-  var wagerNotes = $('.wager-notes')
-  var questionButton = $('.question')
+  var questionButton = $('.question');
 
-  questionButton.on("click", function() {
-    $(this).parents('#new_proposed_wagers').find('.wager-notes').toggle()
+  questionButton.on("click", function () {
+    $(this).parents('#new_proposed_wagers').find('.wager-notes').toggle();
   })
 
 
