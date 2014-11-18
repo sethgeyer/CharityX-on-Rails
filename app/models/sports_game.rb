1bbbc654510e477a5f4ebd632bb02e922b23847a
psylinse <jeff@jetaggart.com>
2014-11-18 13:32:51 -0700

    Remove dead code

--- app/models/sports_game.rb
+++ app/models/sports_game.rb
@@ -4,33 +4,4 @@ class SportsGame < ActiveRecord::Base
     where('date > ?', DateTime.now.utc)
   end
 
-
-  # def self.get_final_score(week_number, vs_id, home_id)
-  #   response = JSON.parse(get("/nfl-t1/2014/REG/#{week_number}/#{vs_id}/#{home_id}/boxscore.json?api_key=#{ENV["SD_NFL_KEY"]}").body)
-  #   home_score = response["home_team"]["points"]
-  #   visitor_score = response["away_team"]["points"]
-  #   status = response["status"]
-  #   quarter = response["quarter"]
-  #   clock = response["clock"]
-  #   if status == "closed"
-  #     if home_score > visitor_score
-  #       response["home_team"]["id"]
-  #     else
-  #       response["away_team"]["id"]
-  #     end
-  #   else
-  #     nil
-  #   end
-  #end
-
-
-
-
-
-
-
-
-
-
-
 end
